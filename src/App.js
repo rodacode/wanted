8bf45999e9864e036d3124f59e44af9e5fa6c665
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2020-01-26 14:04:05 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Grommet } from 'grommet';
+import Home from './containers/Home';
 
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grommet theme={theme}>
+      <Home />
+    </Grommet>);
 }
 
 export default App;
